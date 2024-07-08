58cdc5b9a10ccb34cb137ec2961bd71a4979504f
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-07-08 18:13:00 -0500

    Definiendo la funcion

--- src/Componentes/Boton.js
+++ src/Componentes/Boton.js
@@ -1,6 +1,14 @@
-import react from 'react';
+import React from 'react';
 
 function Boton (props) {
+
+
+    const esOperador = valor => {
+        return isNaN(valor) && (valor != '.') && (valor != '=');
+    }
+
+
+
     return (
         <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
             {props.children}
