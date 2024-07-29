04f17df9eeff9c54dc85735f39ea7b3a43ba9992
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-07-29 13:21:49 -0500

    Los botones ya se pueden observar en la pantalla de la calculadora

--- src/Componentes/Boton.js
+++ src/Componentes/Boton.js
@@ -11,7 +11,8 @@ function Boton (props) {
 
 
     return (
-        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd() }
+        onClick={() => props.manejarClic(props.children)}>
             {props.children}
         </div>
     );
