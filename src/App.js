2b0337e4c5a202882662133d701776c3b22635bd
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-07-30 16:10:04 -0500

    Arreglamos un error que habia en el igual

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ function App() {
   };
 
   const calcularResultado = () => {
-    setInput(evaluate(input));
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert('Por favor ingrese valores para realizar los calculos.')
+    }
   }
 
 
